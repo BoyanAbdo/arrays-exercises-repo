dfbf9d0e8a9c33aee1e273c9c482955f79a22cce
Boyan Abdo <boyan.abdo@gmail.com>
2017-09-30 11:23:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.EqualSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sumLeft = 0;
+            int sumRight = 0;
+            bool findSomething = false;
+
+            if (numbers.Length == 1)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        sumLeft += numbers[j];
+                    }
+                    for (int k = i + 1; k < numbers.Length; k++)
+                    {
+                        sumRight += numbers[k];
+                    }
+
+                    if (sumLeft == sumRight)
+                    {
+                        Console.WriteLine(i);
+                        findSomething = true;
+                    }
+                    else
+                    {
+                        sumLeft = 0;
+                        sumRight = 0;
+                    }
+                }
+
+                if (findSomething == false)
+                {
+                    Console.WriteLine("no");
+                }
+            }
+
+
+
+
+        }
+    }
+}
